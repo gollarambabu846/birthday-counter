6da3e5de25ab46e018c2267a7c05384be79cfaaf
Golla Rambabu <116589566+gollarambabu846@users.noreply.github.com>
2024-11-01 09:07:20 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'November 16, 2024',
+  name: 'Triveni (Miss India)'
 };
 
 function hideEverything() {
